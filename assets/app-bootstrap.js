1d998dc9071901af03b7f8b9d94780bd904f0e58
w3v1n4t0r-hue <w3v1n4t0r@gmail.com>
2026-09-21 23:22:42 -0500

    Avoid duplicate shared state runtime injection

--- assets/app-bootstrap.js
+++ assets/app-bootstrap.js
@@ -1,4 +1,4 @@
-(()=>{const s=document.createElement('script');s.src='/assets/system-states.js';s.defer=true;document.head.appendChild(s)})();
+(()=>{if(document.querySelector('script[src*="/assets/system-states.js"]'))return;const s=document.createElement('script');s.src='/assets/system-states.js';s.defer=true;document.head.appendChild(s)})();
 (()=>{
 'use strict';
 if(window.__pilotDeskAppBootstrap)return;window.__pilotDeskAppBootstrap=true;
